76c7877e816ee941467439d0801b26f3b8b83cbc
intercalaris <ymaviv@gmail.com>
2024-10-16 13:50:07 -0400

    complete

--- js/main.js
+++ js/main.js
@@ -1,27 +1,4 @@
 import {apiKey} from "./apiKey.js";
-// document.querySelector('button').addEventListener('click', getPicOfDay)
-// function getPicOfDay(){
-//     const userInput = document.querySelector('input').value
-//     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`
-//     const url = 'https://api.nasa.gov/'
-
-//     fetch(url)
-//         .then(res => res.json()) // parse response as JSON
-//         .then(data => {
-//             console.log(data)
-//             console.log(data.drinks[0].strDrink)
-//             console.log(data.drinks[0].strDrinkThumb)
-//             console.log(data.drinks[0].strInstructions)
-
-//             document.querySelector('h2').innerText = data.drinks[0].strDrink
-//             document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//             document.querySelector('h3').innerText = data.drinks[0].strInstructions
-//         })
-//         .catch(err => {
-//         console.log(`error ${err}`)
-//     });
-// }
-//The user will enter a date. Use that date to get the NASA picture of the day from that date! https://api.nasa.gov/
 document.querySelector('button').addEventListener('click', getTemperature)
 // https://www.weatherbit.io/api/weather-current
 
